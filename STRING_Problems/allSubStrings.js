adcc360f45ceb1b74d1b514f0fe3eb26b5f086d9
Neeraj Kumar <amck.sharma@gmail.com>
2026-01-08 13:33:41 +0530

    All Sub-string done

---
+++ STRING_Problems/allSubStrings.js
@@ -0,0 +1,13 @@
+
+function allSubStrings (str) {
+    for (let i = 0; i < str.length; i++) {
+        let result = "";
+
+        for (let j = i; j < str.length; j++) {
+            result += str[j];
+            console.log(result);
+        }
+    }
+}
+
+allSubStrings('babacabc');
