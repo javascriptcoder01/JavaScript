32b22036136c0def6f8240b5932a6c58cb44f1ac
Neeraj Kumar <amck.sharma@gmail.com>
2026-01-13 15:51:30 +0530

    Insert Element in the Right position Done

---
+++ ARRAY_Problems/insertElementRightPosition.js
@@ -0,0 +1,27 @@
+
+let arr = [1, 2, 3, 5, 6, 7];
+let value = 4;
+
+for (let i = arr.length - 1; i >= 0; i--) {    // 3
+
+    if (arr[i] > value) {                   // 5 > 4 --> shift element
+
+        arr[i + 1] = arr[i];
+    } else {                            // 5 < 4  --> insert the element
+        arr[i + 1] = value;
+        break;                        // prevent extra inserting element
+    }
+}
+
+console.log(arr);
+
+// Using While
+let i = arr.length - 1;
+while (i >= 0 && arr[i] > value) {
+    arr[i + 1] = arr[i];
+    i--;
+}
+
+arr[i + 1] = value;
+
+console.log(arr);
