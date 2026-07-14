6cd24fc49b3dfd82a68601129b649ce13405b7db
Neeraj Kumar <amck.sharma@gmail.com>
2026-07-14 08:28:26 +0530

    added left and right rotation

--- ARRAY_Problems/leftRotation.js
+++ ARRAY_Problems/leftRotation.js
@@ -1,5 +1,35 @@
+function rotateLeft(arr, k) {
+    let n = arr.length;
+    k = k % n;
 
-function leftRotation (arr, position) {
+    // Reverse- Whole Array
+    reverse(arr, 0, n - 1);
+
+    // Reverse - k element
+    reverse(arr, 0, k - 1);
+
+    // Reverse - Remaining element
+    reverse(arr, k, n - 1);
+
+    return arr;
+}
+
+function reverse(arr, start, end) {
+    while (start < end) {
+        let temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+let arr = [1, 2, 3, 4, 5];
+const result = rotateLeft(arr, 3);
+console.log(result);
+
+
+function leftRotation(arr, position) {
     let l = arr.length;
 
     position = position % l; // In case position is greater than array length
@@ -7,13 +37,13 @@ function leftRotation (arr, position) {
     for (let i = 0; i < position; i++) {
         let firstElement = arr[0];
 
-        for ( let j = 0; j < l - 1; j++) {
-            arr[j] = arr[j + 1];
+        for (let j = 0; j < l - 1; j++) {
+            arr[j] = arr[j + 1]; // make 2nd element for just first element
         }
 
-        arr[l-1] = firstElement;
+        arr[l - 1] = firstElement; // make first for last element
     }
     return arr;
 }
 
-console.log(leftRotation([1, 2, 3, 4, 5], 3)); // [3,4,5,1,2]
+console.log(leftRotation([1, 2, 3, 4, 5], 2)); // [3,4,5,1,2]
--- ARRAY_Problems/rightRotation.js
+++ ARRAY_Problems/rightRotation.js
@@ -1,3 +1,29 @@
+function rightRotate(arr, k) {
+    let n = arr.length;
+
+    k = k % n;
+
+    
+    reverse(arr, 0, n - 1); // Reverse - Whole Array   
+    reverse(arr, 0, k - 1); // Reverse - k element  
+    reverse(arr, k, n - 1); // Reverse - Remeaining Element
+
+    return arr;
+}
+
+let arr = [1, 2, 3, 4, 5];
+let result = rightRotate(arr, 2);
+console.log(result);
+
+function reverse(arr, start, end) {
+    while (start < end) {
+        let temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
 
 function rightRotation(arr, position) {
     let l = arr.length;
@@ -19,4 +45,4 @@ function rightRotation(arr, position) {
     return arr;
 }
 
-console.log(rightRotation([1, 2, 3, 4, 5], 2)); // [4,5,1,2,3]
+console.log(rightRotation([1, 2, 3, 4, 5], 3)); // [4,5,1,2,3]
